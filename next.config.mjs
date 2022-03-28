ebe3c17a75da9199b7ce25858d92f9fab5b83770
jari <jarizw@gmail.com>
2022-03-28 17:58:02 +0200

    fix: create redirects for public files. dumb firefox fix.

--- next.config.mjs
+++ next.config.mjs
@@ -2,12 +2,16 @@ import locales from './src/const/locales.mjs';
 import svgr from 'next-plugin-svgr';
 import withPlugins from 'next-compose-plugins';
 import images from 'next-images';
+import { globby } from 'globby';
+import { relative, join } from 'path';
+
+export const defaultLocale = 'en';
 
 export const nextConfig = {
     trailingSlash: true,
     i18n: {
         locales,
-        defaultLocale: 'en',
+        defaultLocale,
     },
     experimental: {
         outputStandalone: true,
@@ -16,6 +20,23 @@ export const nextConfig = {
         // this does mean the non-translated pages build slower, but eh.
         cpus: 1,
         workerThreads: false,
+    },
+    async redirects() {
+        // Redirects public files that were accidentally prefixed with a locale.
+        const paths = await globby('./public/**/*', { expandDirectories: false });
+        return locales.flatMap((locale) =>
+            locale !== defaultLocale
+                ? paths.map((path) => {
+                      const relativePath = join('/', relative('./public', path));
+                      return {
+                          source: `/${locale}${relativePath}`,
+                          destination: relativePath,
+                          permanent: false,
+                          locale: false,
+                      };
+                  })
+                : []
+        );
     },
 };
 
