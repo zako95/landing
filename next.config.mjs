cab8658f4ab752dae676edbf23f7826e1b538e12
Jari Zwarts <jarizw@gmail.com>
2022-04-23 17:16:47 +0200

    fix: add max-age=7200 for images

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,21 @@ export const nextConfig = {
         cpus: 1,
         workerThreads: false,
     },
+    async headers() {
+        // https://stackoverflow.com/questions/62077589/setting-cache-control-header-for-static-file-serving-on-nextjs-default-server
+        return [
+            {
+                source: '/:all*(svg|jpg|png)',
+                locale: false,
+                headers: [
+                    {
+                        key: 'Cache-Control',
+                        value: 'public, max-age=7200, must-revalidate',
+                    }
+                ],
+            },
+        ]
+    },
     async redirects() {
         // Redirects public files that were accidentally prefixed with a locale.
         const paths = await globby('./public/**/*', { expandDirectories: false });
